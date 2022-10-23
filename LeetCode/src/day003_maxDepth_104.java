57d1885b12f246e8385ec7caacbd20be2b9fbaed
Yaxar <nire_automate@163.com>
2022-10-23 18:19:07 +0800

    Initial commit

--- LeetCode/src/day003_maxDepth_104.java
+++ LeetCode/src/day003_maxDepth_104.java
@@ -2,6 +2,9 @@ import structure.TreeNode;
 
 public class day003_maxDepth_104 {
     public int maxDepth(TreeNode root) {
-        return 0;
+        if(root==null){
+            return 0;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right))+1;
     }
 }
--- LeetCode/src/day004_divide_29.java
+++ LeetCode/src/day004_divide_29.java
@@ -1,4 +1,4 @@
-public class day002_divide_29 {
+public class day004_divide_29 {
     public int divide(int dividend, int divisor) {
       return 0;
     }
